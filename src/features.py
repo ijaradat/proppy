19e91c80a59d0cb5eacd1a37340c9da04bd7d1a0
ijaradat <ijaradat@qf.org.qa>
2018-04-24 13:30:48 +0300

    added general funcs to extract lexicon features

--- src/features.py
+++ src/features.py
@@ -1,9 +1,12 @@
 from setup import load_datset
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize import word_tokenize
+import codecs
+
 
 print ('initialization: ')
 xtrain = load_datset('../data/xtrain.txt')
-vectorizer =  TfidfVectorizer(analyzer="word",tokenizer=None,ngram_range=(1, 3))
+vectorizer =  TfidfVectorizer(analyzer="word",ngram_range=(1, 3))
 documents = [doc.text for doc in xtrain]
 vectorizer.fit_transform(documents)
 
@@ -13,3 +16,23 @@ def extract_baseline_feature(ds):
     for doc in ds:
         doc.baseline_feature = vectorizer.transform([doc.text])
     return vectorizer
+
+
+def extract_from_lexicon(ds,lexicon):
+    for doc in ds:
+        tokens = word_tokenize(doc.text.lower())
+    count = 0
+    for word in lexicon:
+        count += tokens.count(word)
+    return count
+
+
+def load_lexicon(file):
+    lexicon=[]
+    with codecs.open(file,'r') as f:
+        for line in f:
+            line=line.strip()
+            lexicon.append(line)
+        f.close()
+        return lexicon
+
