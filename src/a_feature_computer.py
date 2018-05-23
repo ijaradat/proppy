08e12f4f8137204b15c4b7171cf9a4aad6a1b4db
Alberto Barron <albarron@gmail.com>
2018-05-23 09:39:54 +0300

    the readers in the feat computer now come from setup

--- src/a_feature_computer.py
+++ src/a_feature_computer.py
@@ -15,63 +15,63 @@ from sklearn.preprocessing import MaxAbsScaler
 
 from document import document
 from features import *
-
+from setup import *
 # DEFAULT_SUFFIX = "feats.pickle"
 FORMAT = "%(asctime)-15s %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 
-def load_json_dataset (dataset_file):
-    logging.info('loading dataset: %s', dataset_file)
-    # logging.info ()
-    dataset = []
-    ds = json.load(open(dataset_file))
-    for i, item in enumerate(ds):
-        article = document(item['html_text'], item['propaganda_label'], i)
-        dataset.append(article)
-    logging.info('dataset loaded !')
-    return dataset
+# def load_json_dataset (dataset_file):
+#     logging.info('loading dataset: %s', dataset_file)
+#     # logging.info ()
+#     dataset = []
+#     ds = json.load(open(dataset_file))
+#     for i, item in enumerate(ds):
+#         article = document(item['html_text'], item['propaganda_label'], i)
+#         dataset.append(article)
+#     logging.info('dataset loaded !')
+#     return dataset
+#
+#
+# def load_myds(dataset_file):
+#     logging.info('loading dataset: %s', dataset_file)
+#     dataset = []
+#     with codecs.open(dataset_file, 'r', encoding='utf8') as f:
+#         i = 0
+#         for line in f:
+#             # line= line.strip()
+#             fields = line.split('\t')
+#             article = document(fields[0], fields[-1], i)
+#             dataset.append(article)
+#             i += 1
+#         f.close()
+#     print ('dataset loaded !')
+#     return dataset
+#
+#
+# def load_dataset(dataset_file):
+#     logging.info('loading dataset: %s', dataset_file)
+#     dataset = []
+#     with codecs.open(dataset_file,'r') as f:
+#         i=0
+#         for line in f:
+#             fields = line.split('\t')
+#             article = document(fields[1],fields[0],i)
+#             dataset.append(article)
+#             i+=1
+#         f.close()
+#     logging.info('dataset loaded !')
+#     return dataset
 
 
-def load_myds(dataset_file):
-    logging.info('loading dataset: %s', dataset_file)
-    dataset = []
-    with codecs.open(dataset_file, 'r', encoding='utf8') as f:
-        i = 0
-        for line in f:
-            # line= line.strip()
-            fields = line.split('\t')
-            article = document(fields[0], fields[-1], i)
-            dataset.append(article)
-            i += 1
-        f.close()
-    print ('dataset loaded !')
-    return dataset
-
-
-def load_dataset(dataset_file):
-    logging.info('loading dataset: %s', dataset_file)
-    dataset = []
-    with codecs.open(dataset_file,'r') as f:
-        i=0
-        for line in f:
-            fields = line.split('\t')
-            article = document(fields[1],fields[0],i)
-            dataset.append(article)
-            i+=1
-        f.close()
-    logging.info('dataset loaded !')
-    return dataset
-
-
-def read_datsets(input_file):
-    if input_file.endswith('.json'):
-        dataset = load_json_dataset(input_file)
-    elif input_file.endswith('.converted.txt'):
-        dataset = load_myds(input_file)
-    else:
-        dataset = load_dataset(input_file)
-    return dataset
+# def read_datsets(input_file):
+#     if input_file.endswith('.json'):
+#         dataset = load_json_dataset(input_file)
+#     elif input_file.endswith('.converted.txt'):
+#         dataset = load_myds(input_file)
+#     else:
+#         dataset = load_dataset(input_file)
+#     return dataset
 
 
 # def compute_features(feats, ds, required_feature):
